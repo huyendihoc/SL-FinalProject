10322b3505a0a2666a430a0eaa89a9be671f7cf0
huhyhuvinh <vinhtran048@gmail.com>
2025-08-08 15:40:37 +0700

    [backend] modified autocomplete

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,14 @@
 from flask import Flask, request, jsonify, send_from_directory
 import json
 from flask_cors import CORS, cross_origin
+from flask_caching import Cache
 import csv
 import api
 
 app = Flask(__name__)
 app.config['CORS_HEADERS'] = 'Content-Type'
 cors = CORS(app)
+cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
 @app.route('/')
 def index():
@@ -15,7 +17,14 @@ def index():
 @app.route('/autocomplete/<string:movie>', methods=['GET'])
 @cross_origin()
 def getAutoComplete(movie):
-    return api.autoComplete(movie)
+    cache_key = f'autocomplete_{movie.lower()}'
+    cache_result = cache.get(cache_key)
+    if cache_result:
+        return cache_result
+    
+    results = api.autoComplete(movie)
+    cache.set(cache_key, results, timeout=60*15)
+    return results
 
 @app.route('/imdb/<string:movie>', methods=['GET'])
 @cross_origin()
