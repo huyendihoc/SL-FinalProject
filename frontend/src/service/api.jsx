2ab6f795fc145e3ded3a559f5dcfa4837f2bea8d
huhyhuvinh <vinhtran048@gmail.com>
2025-08-12 18:38:52 +0700

    [frontend] updated frontend api

--- frontend/src/service/api.jsx
+++ frontend/src/service/api.jsx
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 const baseURL = 'http://localhost:5000'
 
-const fetchMovie = async (movie, platform) => {
-    const res = await axios.get(`${baseURL}/${platform}/${movie}`)
+const fetchMovie = async (imdbID, platform) => {
+    const res = await axios.get(`${baseURL}/${platform}/${imdbID}`)
     return res.data 
 }
 
